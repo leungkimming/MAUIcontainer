a7c6698640968a320a0109397a8420b098cd7116
kmleung <kmleung@live.com>
2022-11-27 21:12:51 +0800

    get MyApps List from static website

---
+++ MAUIcontainer/DTOs/MyAppsResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUIcontainer {
+    public class MyAppsResponse {
+        public MyAppDto[] MyAppsDto { get; set; }
+    }
+    public class MyAppDto {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string BlazorNamespace { get; set; }
+        public string BlazorUrl { get; set; }
+        public string ImageBase64 { get; set; }
+        public MyApp toMyApp {
+            get {
+                return new MyApp() {
+                    Name = Name,
+                    Description = Description,
+                    BlazorNamespace = BlazorNamespace,
+                    BlazorUrl = BlazorUrl,
+                    Imagestream = ImageSource.FromStream(() => new MemoryStream(Convert.FromBase64String(ImageBase64)))
+                };
+            }
+        }
+    }
+}
