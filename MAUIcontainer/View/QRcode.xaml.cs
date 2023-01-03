d63e706489f4ec74da914f766422f1a985c042c0
kmleung <kmleung@live.com>
2023-01-03 20:11:31 +0800

    fix qr code drain battery

--- MAUIcontainer/View/QRcode.xaml.cs
+++ MAUIcontainer/View/QRcode.xaml.cs
@@ -20,12 +20,18 @@ public partial class QRcode : ContentPage {
             Multiple = true
         };
     }
+    protected override void OnAppearing() {
+        barcodeView.IsDetecting = true;
+        base.OnAppearing();
+    }
+    protected override void OnDisappearing() {
+        barcodeView.IsDetecting = false;
+        base.OnDisappearing();
+    }
     protected void BarcodesDetected(object sender, BarcodeDetectionEventArgs e) {
         foreach (var barcode in e.Results) {
-            //Console.WriteLine($"Barcodes: {barcode.Format} -> {barcode.Value}");
             Dispatcher.Dispatch(() => {
                 if (!ScannedList.Any(x => x.Value == barcode.Value)) {
-                    //DisplayAlert("Debug", $"{barcode.Value} ", "Ok");
                     ScannedList.Add(new ScannedCode() {
                         Value = barcode.Value,
                         Type = barcode.Format.ToString(),
@@ -35,7 +41,6 @@ public partial class QRcode : ContentPage {
         }
     }
     void Cancel_Clicked(object sender, EventArgs e) {
-        //barcodeView.IsDetecting = false;
         ScannedList.Clear();
         MessagingCenter.Send<QRcode, ObservableCollection<ScannedCode>>(this, "ScanCode", ScannedList);
         Navigation.PopModalAsync();
